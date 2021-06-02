0afc93d775f14332a2de7e120b379e0857ad1b13
Phantom <forsem1234@gmail.com>
2021-06-01 23:03:10 -0400

    True React setup, integration with flask successful

--- gpu-monitor/src/App.js
+++ gpu-monitor/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [initialData, setInitialData] = useState([{}]);
+
+    useEffect(() => {
+      fetch("/api").then(
+        response => response.json()
+      ).then(data => setInitialData(data))
+    }, [])
+    return (
+        <div className="App">
+            <h1>{initialData.hi}</h1>
+        </div>
+    )
 }
-
-export default App;
