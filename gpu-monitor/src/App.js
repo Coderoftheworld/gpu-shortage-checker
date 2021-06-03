815e7ad56973552480babab38c2667db9845d581
Phantom <forsem1234@gmail.com>
2021-06-03 08:04:36 -0400

    not working!!!

--- gpu-monitor/src/App.js
+++ gpu-monitor/src/App.js
@@ -1,17 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import './App.css'
+import React from 'react';
+import { render } from 'react-dom';
+import ReactDOM from 'react-dom'
+import { Grid, Typography, Button, ButtonGroup } from '@material-ui/core';
 
 export default function App() {
-    const [initialData, setInitialData] = useState([{}]);
-
-    useEffect(() => {
-      fetch("/api").then(
-        response => response.json()
-      ).then(data => setInitialData(data))
-    }, [])
     return (
-        <div className="App">
-            <h1>{initialData.hi}</h1>
-        </div>
+        <Grid
+            container
+        ><p>Hello</p>
+        </Grid>
     )
 }
+const rootDiv = document.getElementById('root')
+render(<App />, rootDiv)
